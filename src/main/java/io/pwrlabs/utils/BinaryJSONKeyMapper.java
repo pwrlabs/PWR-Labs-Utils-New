3c97f8a3d1e7e56b371dcbc7cc708d87c0679269
eaoe3 <haddadedy82@gmail.com>
2025-04-11 18:50:13 +0300

    Add PROCESS_WITHOUT_CRITICAL_ERRORS key in BinaryJSONKeyMapper

--- src/main/java/io/pwrlabs/utils/BinaryJSONKeyMapper.java
+++ src/main/java/io/pwrlabs/utils/BinaryJSONKeyMapper.java
@@ -134,6 +134,7 @@ public class BinaryJSONKeyMapper {
     public static final String BLOCKCHAIN_VERSION = "blockchainVersion";
     public static final String BLOCK_REWARD = "blockReward";
     public static final String BURNED_FEES = "burnedFees";
+    public static final String PROCESS_WITHOUT_CRITICAL_ERRORS = "processedWithoutCriticalErrors";
 
     public static final String TITLE = "title";
     public static final String DESCRIPTION = "description";
@@ -205,6 +206,8 @@ public class BinaryJSONKeyMapper {
         addKey(BLOCKCHAIN_VERSION);
         addKey(BLOCK_REWARD);
         addKey(BURNED_FEES);
+        addKey(PROCESS_WITHOUT_CRITICAL_ERRORS);
+
 
         addKey(TITLE);
         addKey(DESCRIPTION);
