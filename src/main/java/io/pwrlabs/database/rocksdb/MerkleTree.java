2b96cb6346fbb3ffdf00c607f34f44f8006dbbea
eaoe3 <haddadedy82@gmail.com>
2025-05-07 22:04:28 +0300

    Clear keyDataCache in MerkleTree after processing nodes to improve resource management

--- src/main/java/io/pwrlabs/database/rocksdb/MerkleTree.java
+++ src/main/java/io/pwrlabs/database/rocksdb/MerkleTree.java
@@ -406,6 +406,7 @@ public class MerkleTree {
                 }
 
                 nodesCache.clear();
+                keyDataCache.clear();
             }
         } finally {
             lock.writeLock().unlock();
